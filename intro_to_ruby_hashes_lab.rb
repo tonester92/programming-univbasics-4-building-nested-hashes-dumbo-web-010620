fad9e22edc757b7f9cbce413db4ff2c33e1bcfa3
Antonio Blandin <antonio.blandin25@gmail.com>
2019-12-18 23:46:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+<<<<<<< HEAD
    monopoly = {railroads: {
     pieces: 4,
     rent_in_dollars: {one_piece_owned: 25,
@@ -32,6 +33,40 @@ def monopoly_with_third_tier
       shortline_railroad: {}
     }
   }}
+=======
+   monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
+>>>>>>> b756099afe4263e51c4c2beadf4f51220076aa3f
 end
 
 def monopoly_with_fourth_tier
@@ -64,12 +99,21 @@ def monopoly_with_fourth_tier
       {
         "mortgage_value"=>"$400"
       },
+<<<<<<< HEAD
       :shortline_railroad=>
       {
         "mortgage_value"=> "$800"
+=======
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+>>>>>>> b756099afe4263e51c4c2beadf4f51220076aa3f
       }
     }
   }
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> b756099afe4263e51c4c2beadf4f51220076aa3f
 end
